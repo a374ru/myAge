59f9417239bfc1c47cdfc83a1bb387a3b74beca4
Oleg <82386871+a374ru@users.noreply.github.com>
2023-09-29 09:25:41 +0300

    000

---
+++ src/app/edge/edge.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+
+@Component({
+  selector: 'app-edge',
+  templateUrl: './edge.component.html',
+  styleUrls: ['./edge.component.scss']
+})
+export class EdgeComponent {
+
+  resultEdge = "";
+  otvet = ""
+  messages = [
+    "Прожитых дней: ",
+    "Невозможно прожить дальше текущего дня!",
+    "green",
+    "red"
+  ];
+  postComment = "штук.";
+classColor =""
+  calkEdge(rrr: string) {
+    if (rrr) {
+      const date = new Date().getTime()
+      let dr = new Date(rrr).getTime()
+      const res = Math.trunc((date - dr) / 86400 / 1000)
+      if (res > 0) {
+        this.resultEdge = res.toLocaleString()
+        this.otvet = this.messages[0]
+        this.resultEdge = String(res)
+        this.classColor = this.messages[2]
+      } else {
+        this.otvet = this.messages[1]
+        this.classColor = "red"
+
+        this.resultEdge = ""
+      }
+    }
+  }
+}
